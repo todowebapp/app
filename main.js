92fddf00fecd4f95358fd5b56731edf29bcc8cb2
todowebapp <todoapp.github@proton.me>
2024-12-25 18:10:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,216 @@
+class Entry
+{
+    constructor(text, completed = false) {
+        this.text = text;
+        this.completed = completed;
+    }
+    
+    setCompleted(completed) {
+        this.completed = completed;
+    }
+    
+    toggleCompleted() {
+        this.completed = !this.completed;
+    }
+};
+
+
+class ToDoList
+{
+    constructor(name) {
+        this.name = name;
+        this.entries = [];
+    }
+    
+    addEntry(entry) {
+        this.entries.push(entry);
+    }
+    
+    createEntry(text, completed = false) {
+        const entry = new Entry(text, completed);
+        this.entries.push(entry);
+        return entry;
+    }
+};
+
+
+const sideBarNewListBtn = document.getElementById("sideBar-newListBtn");
+const sideBarListRefs = document.getElementById("sideBar-listRefs");
+const toDoListWin = document.getElementById("toDoListWin");
+const toDoListWinListName = document.getElementById("toDoListWin-listName");
+const toDoListWinEntries = document.getElementById("toDoListWin-entries");
+const toDoListWinNewEntryBtn = document.getElementById("toDoListWin-newEntryBtn");
+const listCreationWin = document.getElementById("listCreationWin");
+const listCreationWinNameTxtField = document.getElementById("listCreationWin-nameTxtField");
+const listCreationWinCancelBtn = document.getElementById("listCreationWin-cancelBtn");
+const listCreationWinAddBtn = document.getElementById("listCreationWin-addBtn");
+const sideBarListRefsItemTemplate = document.getElementById("sideBar-listRefs-item-template");
+const toDoListWinEntryTemplate = document.getElementById("toDoListWin-entry-template");
+
+const LOCAL_STORAGE_TO_DO_LISTS_KEY = "toDoLists";
+const LOCAL_STORAGE_CURRENT_TO_DO_LIST_KEY = "currentToDoList";
+
+
+function renderToDoListRef(toDoList) {
+    const sideBarListRefsItem = sideBarListRefsItemTemplate.content.cloneNode(true).querySelector(".sideBar-listRefs-item");
+    
+    sideBarListRefsItem.innerText = toDoList.name;
+    sideBarListRefsItem.addEventListener("click", function() {
+        renderToDoList(toDoList);
+    });
+    
+    sideBarListRefs.appendChild(sideBarListRefsItem);
+}
+
+
+function renderEntry(entry) {
+    const entryDiv = toDoListWinEntryTemplate.content.cloneNode(true).querySelector(".toDoListWin-entry");
+    const entryCheckbox = entryDiv.querySelector(".toDoListWin-entry-checkbox");
+    const entryTextDiv = entryDiv.querySelector(".toDoListWin-entry-text");
+    
+    entryCheckbox.checked = entry.completed;
+    entryCheckbox.addEventListener("change", function(event) {
+        entry.completed = event.target.checked;
+    });
+    
+    entryTextDiv.innerText = entry.text;
+    entryTextDiv.addEventListener("input", function(event) {
+        entry.text = event.target.innerText;
+    });
+    
+    toDoListWinEntries.appendChild(entryDiv);
+    return entryDiv;
+}
+
+
+function renderToDoList(toDoList) {
+    // clear
+    toDoListWinEntries.innerHTML = "";
+    
+    // render list name
+    toDoListWinListName.innerText = toDoList.name;
+    
+    // render entries
+    for (let i = 0; i < toDoList.entries.length; i++) {
+        const entry = toDoList.entries[i];
+        renderEntry(entry);
+    }
+    
+    // show
+    toDoListWin.style.display = "flex";
+}
+
+function hideToDoListWin() {
+    toDoListWin.style.display = "none";
+}
+
+
+function showListCreationWin() {
+    listCreationWinNameTxtField.value = "";
+    listCreationWin.hidden = false;
+}
+
+
+function hideListCreationWin() {
+    listCreationWin.hidden = true;
+}
+
+
+function createToDoList(listName, toDoLists) {
+    const toDoList = new ToDoList(listName);
+    
+    toDoLists.push(toDoList);
+    renderToDoListRef(toDoList);
+    
+    return toDoList;
+}
+
+
+function saveToDoLists(toDoLists) {
+    const toDoListsStr = JSON.stringify(toDoLists);
+    localStorage.setItem(LOCAL_STORAGE_TO_DO_LISTS_KEY, toDoListsStr);
+}
+
+
+function loadToDoLists() {
+    const toDoListsStr = localStorage.getItem(LOCAL_STORAGE_TO_DO_LISTS_KEY);
+    if (toDoListsStr === null) return [];
+    
+    const toDoListsData = JSON.parse(toDoListsStr);
+    return toDoListsData.map(listData => {
+        const toDoList = new ToDoList(listData.name);
+        toDoList.entries = listData.entries.map(entryData => new Entry(entryData.text, entryData.completed));
+        return toDoList;
+    });
+}
+
+
+function saveCurrentToDoList(currentToDoList) {
+    const currentToDoListStr = JSON.stringify(currentToDoList);
+    localStorage.setItem(LOCAL_STORAGE_CURRENT_TO_DO_LIST_KEY, currentToDoListStr);
+}
+
+
+function loadCurrentToDoList() {
+    const currentToDoListStr = localStorage.getItem(LOCAL_STORAGE_CURRENT_TO_DO_LIST_KEY);
+    if (currentToDoListStr === null) return null;
+    
+    const listData = JSON.parse(currentToDoListStr);
+    const toDoList = new ToDoList(listData.name);
+    toDoList.entries = listData.entries.map(entryData => new Entry(entryData.text, entryData.completed));
+    return toDoList;
+}
+
+
+function main() {
+    // vars
+    let toDoLists = loadToDoLists();
+    let currentToDoList = loadCurrentToDoList();
+    
+    // add event listeners
+    
+    window.addEventListener("beforeunload", function() {
+        saveToDoLists(toDoLists);
+        saveCurrentToDoList(currentToDoList);
+    });
+    sideBarNewListBtn.addEventListener("click", showListCreationWin);
+    toDoListWinNewEntryBtn.addEventListener("click", function() {
+        if (!currentToDoList) return;
+        
+        const newEntry = currentToDoList.createEntry("");
+        const entryDiv = renderEntry(newEntry);
+        entryDiv.focus();
+    });
+    listCreationWinNameTxtField.addEventListener("keydown", function(event) {
+        if (event.key !== "Enter") {
+            return;
+        }
+        
+        const newToDoListName = listCreationWinNameTxtField.value;
+        
+        if (newToDoListName === "") {
+            return;
+        }
+        
+        currentToDoList = createToDoList(newToDoListName, toDoLists);
+        hideListCreationWin();
+        renderToDoList(currentToDoList);
+    });
+    listCreationWinCancelBtn.addEventListener("click", hideListCreationWin);
+    listCreationWinAddBtn.addEventListener("click", function() {
+        const newToDoListName = listCreationWinNameTxtField.value;
+        
+        if (newToDoListName === "") {
+            return;
+        }
+        
+        currentToDoList = createToDoList(newToDoListName, toDoLists);
+        hideListCreationWin();
+        renderToDoList(currentToDoList);
+    });
+    
+    // main code
+    if (currentToDoList !== null) {
+        renderToDoList(currentToDoList);
+    }
+}
